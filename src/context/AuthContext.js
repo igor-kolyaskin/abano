4fc7a2e6265dcf9d085599ced8fea2e1c4218c02
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2024-04-08 09:23:47 +0400

    feat: create AuthContext

---
+++ src/context/AuthContext.js
@@ -0,0 +1,18 @@
+import { createContext, useMemo, useState } from 'react'
+import FirebaseAuth from '../handlers/auth'
+
+const { signIn, signOut } = FirebaseAuth
+const Context = createContext()
+
+const Provider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null)
+  const login = () => {}
+  const logout = () => {}
+
+  const value = useMemo(() => {
+    return { login, logout, currentUser }
+  }, [])
+
+  return <Context.Provider value={value}>{children}</Context.Provider>
+}
+export default Provider
