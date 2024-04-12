d7c55e4ba8237af03626091a2f91abb17e43bb4d
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2024-04-12 23:07:33 +0400

    feat: use hook useNavigation

--- src/components/Single.js
+++ src/components/Single.js
@@ -1,15 +1,20 @@
 import { useLocation, useNavigate } from 'react-router-dom'
-import { useFirestoreContext } from '../context/FirestoreContext'
+import { useFireStoreContext } from '../context/FirestoreContext'
 import Card from './Card'
 
 const Single = () => {
+  const navigate = useNavigate()
+  const { state } = useFireStoreContext()
+  const { state: routerState } = useLocation()
+  const item = state.items.find(item => item.id === routerState.id)
+
   return (
     <>
       <button className='btn btn-link' onClick={() => {}}>
         Back
       </button>
       <div className='d-flex justify-content-center mb-5'>
-        <Card />
+        <Card {...item} />
       </div>
     </>
   )
