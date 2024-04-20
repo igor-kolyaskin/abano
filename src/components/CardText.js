bf634a0e9640f4b6c11ee85f3748a98d60de183a
igor-kolyaskin <131876836+igor-kolyaskin@users.noreply.github.com>
2024-04-20 20:31:51 +0400

    refactor (tbilisi-1): change text for CardText from map to string

--- src/components/CardText.js
+++ src/components/CardText.js
@@ -1,24 +1,48 @@
 function CardText({ data }) {
-  let cardText = null
-  console.log(data)
-  if (data && typeof data === 'string') {
-    cardText = <div className='text-start ps-1 fs-6 mt-1'>{data}</div>
-  } else if (data && Array.isArray(data)) {
-    cardText = (
+  const cardText =
+    data && data.includes('|&|') ? (
+      <ParsedText text={data} />
+    ) : (
+      <div className='text-start ps-1 fs-6 mt-1'>{data}</div>
+    )
+
+  function ParsedText({ text }) {
+    const compArray = text.split('|&|')
+    const parsedText = (
       <div className='text-start ps-1 fs-6 mt-1'>
-        {data?.map(e => {
-          switch (e.type) {
+        {compArray.map((compString, index) => {
+          const compParts = compString.split('|')
+          switch (compParts[0]) {
             case 'span':
-              return <span>{e.innerText}</span>
+              return <span index={index}>{compParts[1]}</span>
             case 'a':
-              return <a href={e.href}>{e.innerText}</a>
+              return (
+                <a index={index} href={compParts[2]}>
+                  {compParts[1]}
+                </a>
+              )
             default:
-              return <div>Error</div>
+              return <div index={index}>{compParts[1]}</div>
           }
         })}
       </div>
     )
+
+    return parsedText
   }
   return cardText
 }
 export default CardText
+
+const example =
+  `span|Вот отличное 
+  |&|
+  a|видео|https://www.youtube.com/watch?v=lfyYKPD0h0Q
+  |&|
+  span| про это заведение`
+// convention:
+// 1) components devider is |&|
+// 2) inside a component devider is |
+// 3) the first part is component type like span, div, a
+// 4) the second part is innerText
+// 5) the third part is href for a
